43e7fa4b27d989470d81477aa0f1842b5308b89d
8KDb12Br <rafb147369@gmail.com>
2022-08-25 13:14:21 +0800

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,7 +12,9 @@ const Home = () => {
   const fetchNFTs = async() => {
     let nfts; 
     console.log("fetching nfts");
-    const api_key = "67cOOUROEGg7FApnc_U-ihL43EZGqYsl"
+    // const api_key = "67cOOUROEGg7FApnc_U-ihL43EZGqYsl"
+    // const api_key = ""
+    const api_key = "ERCMjEAVoJkv1UTDQEPquHUPk8TBBV2O"
     const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTs/`;
     var requestOptions = {
         method: 'GET'
